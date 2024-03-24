711529e7e86b24289f4d9028ce588cf6cd90f410
raviteja-1819 <163375780+raviteja-1819@users.noreply.github.com>
2024-03-24 21:16:00 +0530

    added cors

--- sample.js
+++ sample.js
@@ -21,7 +21,13 @@ if (cluster.isMaster) {
 } else {
   const app = express();
   // Middleware
-  // app.use(cors());
+  app.use(cors());
+  app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+  });
   app.use(bodyParser.json());
   // mysql connection
   const connection = mysql.createConnection({
